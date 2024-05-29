1d4321905933651a244287d06e55985d1980ae84
Alexandra Petrea <alexandra.petrea1@stud.ubbcluj.ro>
2024-05-29 12:57:43 +0300

    comit update22

--- Repositories/MitarbeiterRepo/TestsMitarbeiterRepo/testMitarbeiterRepo.cpp
+++ Repositories/MitarbeiterRepo/TestsMitarbeiterRepo/testMitarbeiterRepo.cpp
@@ -57,3 +57,103 @@ void test_remove_employee() {
     repo.remove_employee("Korn");
     assert(!repo.search_employee("Korn"));
 }
+
+void test_modify_employee1() {
+    MitarbeiterRepo repo("MitarbeiterRepo/testMitarbeiterRepo/tests.json");
+    Mitarbeiter employee("Mustermann", "Max", "max.mustermann@test.de", "Verkäufer", "MM", "03.07.1990", 2100, "bla", true);
+    repo.add_employee(employee);
+
+    // Modifică salariul angajatului
+    repo.modify_employee("MM", "salary", "3500");
+
+    // Verifică modificarea
+    json employees = repo.get_mitarbeiter_json();
+    bool found = false;
+    for (const auto& obj : employees) {
+        if (obj["abbreviation"] == "MM") {
+            assert(obj["salary"] == 3500);
+            found = true;
+            break;
+        }
+    }
+    assert(found);
+
+    // Adaugă un alt angajat pentru teste suplimentare
+    Mitarbeiter employee2("Pop", "Maria", "maria.pop@test.de", "Manager", "PM", "01.01.1985", 3000, "remarks", true);
+    repo.add_employee(employee2);
+
+    // Modifică abrevierea angajatului
+    repo.modify_employee("PM", "position", "Senior Manager");
+
+    // Verifică modificarea
+    employees = repo.get_mitarbeiter_json();
+    found = false;
+    for (const auto& obj : employees) {
+        if (obj["abbreviation"] == "PM") {
+            assert(obj["position"] == "Senior Manager");
+            found = true;
+            break;
+        }
+    }
+    assert(found);
+
+    // Modifică anul de înmatriculare al unei mașini
+    repo.modify_employee("MM", "birthDate", "01.01.2000");
+
+    // Verifică modificarea
+    employees = repo.get_mitarbeiter_json();
+    found = false;
+    for (const auto& obj : employees) {
+        if (obj["abbreviation"] == "MM") {
+            assert(obj["birthDate"] == "01.01.2000");
+            found = true;
+            break;
+        }
+    }
+    assert(found);
+
+    // Șterge angajatul
+    repo.remove_employee("MM");
+
+    // Verifică ștergerea
+    employees = repo.get_mitarbeiter_json();
+    found = false;
+    for (const auto& obj : employees) {
+        if (obj["abbreviation"] == "MM") {
+            found = true;
+            break;
+        }
+    }
+    assert(!found); // Trebuie să fie false, deoarece angajatul a fost șters
+}
+
+
+void test_verify_employee() {
+    MitarbeiterRepo repo("AutoRepo/TestsAutoRepo/tests.json");
+
+    Mitarbeiter invalid_employee("Pop", "Maria", "maria.pop@test.de", "Manager", "PM", "01.01.1700", 3000, "remarks", true);
+
+    try {
+        repo.add_employee(invalid_employee);
+    } catch (const std::runtime_error& e) {
+        assert(std::string(e.what()) == "Invalid year of birth.");
+    }
+
+    Mitarbeiter invalid_employee2("Pop", "Maria", "maria.pop@test.de", "Manager", "PM", "01.01.1985", 30, "remarks", true);
+
+    try {
+        repo.add_employee(invalid_employee2);
+    } catch (const std::runtime_error& e) {
+        assert(std::string(e.what()) == "Invalid salary.");
+    }
+
+}
+
+
+void run_tests_mitarbeiterr_repo(){
+    test_add_employee();
+    test_remove_employee();
+    test_modify_employee1();
+    test_verify_employee();
+
+}
